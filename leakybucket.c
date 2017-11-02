79ef82aa7d6d3554bf634eb783950a1173e3a89b
Tanishk Sharma <32167236+Tanishk-Sharma@users.noreply.github.com>
2017-11-03 00:47:12 +0530

    Leaky Bucket algorithm
    C implementation for Leaky Bucket algorithm.
    Displays the packets not sent with an 'x' beside them

---
+++ leakybucket.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#define INITIAL 1000
+int main()
+{
+int bucket[20],packets[20],i,spackets,n=INITIAL;
+printf("\nEnter number of Packets:\t");
+scanf("%d",&spackets);
+printf("\nEnter Packet Size:");
+packets[0]=0;
+for(i=1;i<=spackets;i++)
+{
+printf("\nPacket %d:\t",i);
+scanf("%d",&packets[i]);
+}
+bucket[0]=0;
+for(i=1;i<=spackets;i++)
+{
+//printf("\n%d",n);
+if(packets[i]<=n)
+{
+printf("%-3d",i);
+n -= packets[i];
+}
+else if(packets[i]>INITIAL)
+printf("x%-3d",i);
+else
+n=INITIAL;
+n=(n==0)?INITIAL:n;
+}
+return 0;
+}
