9734c013f98dd57abff4c1b6bbec648446250a2c
Tanishk Sharma <32167236+Tanishk-Sharma@users.noreply.github.com>
2017-11-04 04:12:22 +0530

    String encryption by Caeser's cipher
    Encrypting a string using Caeser's cipher encyption algorithm.
    -Implemented in Java.
    -The entered string can contain any symbols, letters. Only letters are shifted, and not the special symbols.
    -The shift variable may be entered so large that it may take new position out of range by multiple times, so to keep shift variable within range, it is stored in another variable "t". If k is greater than 26 or a multiple of it, then k%26 should get it within range.

---
+++ CaeserCipher.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+public class CaeserCipher {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int shift = in.nextInt();
+        StringBuffer str = new StringBuffer();
+        for(char c : s.toCharArray()){
+            if((c>='a' && c<='z') || (c>='A' && c<='Z')){
+                int newchar = (int)c;
+                int t = shift>26 ? shift%26 : shift;
+
+                if(Character.isLowerCase(c))
+                    if(newchar+t>122)
+                        newchar = 96 + ((newchar+t)-122);
+                    else
+                        newchar = newchar+t;
+                
+                else
+                    if(newchar+t>90)
+                        newchar = 64 + ((newchar+t)-90);
+                    else
+                        newchar = newchar+t;
+                
+                str.append((char)newchar);
+            }
+            else
+                str.append(c);
+        }
+        System.out.println(str);
+    }
+}
