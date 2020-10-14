d706727773d4d0be6e50f929a955f7fcda7eaa51
Tanishk Sharma <tanishk9597@gmail.com>
2020-10-14 20:10:49 +0530

    Update and rename 11. CaesarCipher.java to 11. Caesar Cipher/program.java

--- 11. Caesar Cipher/program.java
+++ 11. Caesar Cipher/program.java
@@ -1,11 +1,3 @@
-/*
-Encryption with Caesar code is based on an alphabet shift (move of letters further in the alphabet)
-ie. a same letter is replaced with only one other (always the same for given cipher message). 
-The most commonly used shift/offset is by 3 letters.
-Source: https://www.dcode.fr/caesar-cipher
-*/
-
-
 import java.util.*;
 
 public class CaesarCipher {
