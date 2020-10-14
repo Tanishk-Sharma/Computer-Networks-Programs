b4e577fa93e03dfac5c0c4fc9115d7ab3fdef5db
Tanishk Sharma <tanishk9597@gmail.com>
2020-10-14 20:16:07 +0530

    Update and rename 5. Leaky Bucket.c to 5. Leaky Bucket/program.c

--- 5. Leaky Bucket/program.c
+++ 5. Leaky Bucket/program.c
@@ -1,9 +1,3 @@
-/*
-The Leaky Bucket Algorithm is an algorithm used in packet switched computer networks and telecommunications networks. 
-It can be used to check that data transmissions, in the form of packets, conform to defined limits on bandwidth and burstiness (a measure of the unevenness or variations in the traffic flow). 
-It can also be used as a scheduling algorithm to determine the timing of transmissions that will comply with the limits set for the bandwidth and burstiness(network scheduler). 
-*/
-
 #include<stdio.h>
 #define INITIAL 1000
 
